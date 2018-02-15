ca293be7830fd07f8df1737f2f5381de125de551
victorli0825 <thebrandnewvictor@gmail.com>
2018-02-14 21:48:03 -0800

    initial

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'IjhXXgFYsqImcoFhUq0bWQgai',
+  consumer_secret: 'v3oz816kkOfNzJJ593ERzQvkGdSv5Ns1T54zaqlu48HWQ8tf1D',
+  access_token_key: '963645290816339969-pBnIDc1kyNmgvAHgVugoOUy8aM9C7rJ',
+  access_token_secret: 'vDM1dMRP16q4wKQUgK7a1QeYqQxQvRUQvykd88bLQWvOk',
+}
+
+module.exports = twitterKeys;
